bdc366742f06440c9478f0fca77ca64fc29ef280
leonard4589 <75444828+leonard4589@users.noreply.github.com>
2022-11-11 14:12:28 -0600

    .

--- ass11.py
+++ ass11.py
@@ -1,22 +1,16 @@
 import numpy as np
 import time
 from ezGraph import *
-from bStats import *
-
-# primary equation: hnew = hold + dh
-# filling dh = dt/pi*r^2*Q
-# draining dh = dt/pi*r^2*(-kh)
 
 # Finite Difference Model
-startTime = time.perf_counter()
 
 # Parameters
 dt = 5
-nsteps = 200
+nsteps = 10
 
 
 r = 2.25    # radius (cm)
-Qin = 30    # volume inflow rate (dV/dt): (cubic cm/s)
+Qin = 10    # volume inflow rate (dV/dt): (cubic cm/s)
 h = 0       # initial height (cm)
 k = 0.15     # outflow rate constant
 
@@ -31,16 +25,16 @@ graph = ezGraph(xmin = 0, xmax = 100,
 
 graph.add(0, h)   # add initial values
 
-conv = 0.1
+r = 0.1
+
 # Time loop
 for t in range(1, nsteps):
     modelTime = t * dt
 
-    h_old = h
-
     # Filling
     dh = Qin * dt / (np.pi * r**2)    # find the change in height
     h = h + dh   # update height
+    de = h - r
 
     # Draining
     dVdt = -k * h
@@ -48,26 +42,10 @@ for t in range(1, nsteps):
     h = h + dh
 
     
-    graph.add(modelTime, h)
-    # graph.wait(0.1)
-
-    # finding the change in height
-    dh = h - h_old
-    # stopping the graph at equalibrium
-    if dh < conv:
-        break
+    graph.add(modelTime, de)
+    graph.wait(0.1)
 
 print(h)
 
-endTime = time.perf_counter()
-
-runtime = endTime - startTime
-print (f'runtime: {runtime}')
-
 # Draw graph
 graph.keepOpen()
-
-
-
-
-
