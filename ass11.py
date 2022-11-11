21a0be8c7051fdab4058f919626a79af0f89890e
leonard4589 <75444828+leonard4589@users.noreply.github.com>
2022-11-11 14:45:58 -0600

    .

--- ass11.py
+++ ass11.py
@@ -5,12 +5,12 @@ from ezGraph import *
 # Finite Difference Model
 
 # Parameters
-dt = 5
-nsteps = 10
+dt = 3
+nsteps = 100
 
 
 r = 2.25    # radius (cm)
-Qin = 10    # volume inflow rate (dV/dt): (cubic cm/s)
+Qin = 30    # volume inflow rate (dV/dt): (cubic cm/s)
 h = 0       # initial height (cm)
 k = 0.15     # outflow rate constant
 
@@ -25,27 +25,35 @@ graph = ezGraph(xmin = 0, xmax = 100,
 
 graph.add(0, h)   # add initial values
 
-r = 0.1
+
+
 
 # Time loop
 for t in range(1, nsteps):
     modelTime = t * dt
 
+    Qin = -0.1*modelTime + Qin
+
+    if Qin <= 0:
+        Qin = 0
+
     # Filling
     dh = Qin * dt / (np.pi * r**2)    # find the change in height
     h = h + dh   # update height
-    de = h - r
+    
 
     # Draining
     dVdt = -k * h
     dh = dVdt * dt / (np.pi * r**2)
     h = h + dh
 
-    
-    graph.add(modelTime, de)
+    graph.add(modelTime, h)
     graph.wait(0.1)
 
-print(h)
+    
+        
+
+
 
 # Draw graph
 graph.keepOpen()
