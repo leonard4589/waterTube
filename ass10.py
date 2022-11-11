a18c7dafd91a9ebf7c9d13f2f0e74d0fb449c294
leonard4589 <75444828+leonard4589@users.noreply.github.com>
2022-11-11 14:21:42 -0600

    .

--- ass10.py
+++ ass10.py
@@ -6,11 +6,11 @@ from ezGraph import *
 
 # Parameters
 dt = 5
-nsteps = 10
+nsteps = 200
 
 
 r = 2.25    # radius (cm)
-Qin = 30    # volume inflow rate (dV/dt): (cubic cm/s)
+# Qin = 30    # volume inflow rate (dV/dt): (cubic cm/s)
 h = 0       # initial height (cm)
 k = 0.15     # outflow rate constant
 
@@ -30,19 +30,24 @@ graph.add(0, h)   # add initial values
 for t in range(1, nsteps):
     modelTime = t * dt
 
+    if modelTime <= 50:
+        Qin = 30
+    else:
+        Qin = 0
+
     # Filling
     dh = Qin * dt / (np.pi * r**2)    # find the change in height
     h = h + dh   # update height
 
     # Draining
-    # dVdt = -k * h
-    # dh = dVdt * dt / (np.pi * r**2)
-    # h = h + dh
+    dVdt = -k * h
+    dh = dVdt * dt / (np.pi * r**2)
+    h = h + dh
 
     
     graph.add(modelTime, h)
-    graph.wait(0.1)
-    
+    # graph.wait(0.1)
+
 
 
 # Draw graph
