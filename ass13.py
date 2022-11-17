c562b92925ac078e84e1f9720189127920f71e89
leonard4589 <75444828+leonard4589@users.noreply.github.com>
2022-11-17 13:25:04 -0600

    .

--- ass13.py
+++ ass13.py
@@ -8,7 +8,7 @@ from ezGraph import *
 
 # Parameters
 dt = 1
-nsteps = 200
+nsteps = 100
 
 
 r = 2.25    # radius (cm)
@@ -27,30 +27,38 @@ graph = ezGraph(xmin = 0, xmax = 100,
 
 graph.add(0, h)   # add initial values
 
-Qflag = True
+s_old = 0
 # Time loop
 for t in range(1, nsteps):
     modelTime = t * dt
 
-    Qin = 10*math.sin(5*modelTime) + 11
+    Qin = 10*math.sin(50*modelTime) + 11
 
     if Qin <= 0:
         Qin = 0
 
     # Filling
-    dh = Qin * dt / (np.pi * r**2)    # find the change in height
-    h = h + dh   # update height
+    dhin = Qin * dt / (np.pi * r**2)    # find the change in height
+    h = h + dhin   # update height
 
     # Draining
     dVdt = -k * h
-    dh = dVdt * dt / (np.pi * r**2)
-    h = h + dh
+    dhout = dVdt * dt / (np.pi * r**2)
+    h = h + dhout
 
     
     graph.add(modelTime, h)
     graph.wait(0.1)
 
+    
+    dh = dhin + dhout
+    s = dh/dt
+    if s < 0 and s_old > 0:
+        print (f'Maxium height: {h}')
 
+    elif s > 0 and s_old < 0:
+        print (f'Minimum height: {h}')
+    s_old = s * 1
 
 # Draw graph
 graph.keepOpen()
